9ed01bbb9ed49872dcb0f84500dd0833d44cc1ac
horangifence <6733387@naver.com>
2025-04-01 20:11:09 +0900

    Update NewApp.jsx

--- frontend/src/NewApp.jsx
+++ frontend/src/NewApp.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+
+function NewApp() {
+  return (
+    <div>
+      <h1>Hello, NewApp!</h1>
+      <p>This is your application.</p>
+    </div>
+  );
+}
+
+// default export 추가
+export default NewApp;
 
