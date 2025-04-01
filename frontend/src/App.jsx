992d19f687c20183595aabe5511c60f2ab76be99
horangifence <6733387@naver.com>
2025-04-01 20:06:22 +0900

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to your project!</h1>
+      <p>This is the App component.</p>
+    </div>
+  );
+}
+
+export default App;
